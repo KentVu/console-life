98c1c37c51bfd74b2fa72aa92a7691c43849c7f7
Vu Tran Kien <vutrankien.vn@gmail.com>
2020-08-03 11:20:16 +0900

    Different color for logcat TAG

--- bin/logcat_color.pl
+++ bin/logcat_color.pl
@@ -6,15 +6,17 @@ use warnings;
 my $c_markstart="\e[?7711l";
 my $c_markend="\e[?7711h";
 my %colors = (
-    I => "\e[0;32m",
-    W => "\a$c_markstart\e[0;33m$c_markend",
-    E => "\a$c_markstart\e[0;31m$c_markend",
-    D => "\e[2;37m",
-    V => "\e[2;35m",
-    N => "\e[0;0m"
+    I => "\e[32m",
+    W => "$c_markstart\e[0;33m$c_markend",
+    E => "$c_markstart\e[0;31m$c_markend",
+    D => "",
+    V => "\e[2;37m",
+    n => "\e[0;0m",
+    b => "\e[2m",
 );
 my $dateTime=q/[\d-]+ [\d:\.]+/;
 my $logLvl=q/[VDIWE]/;
+my $focus_pid=$ARGV[0];
 
 sub tidChar {
 	my $tid=$_[0];
@@ -22,12 +24,23 @@ sub tidChar {
 	chr $sum % 64 + ord 'A';
 }
 
-while(<>) {
+sub colorize {
+    my $color = $_[0];
+    $color . $_[1] . $colors{n}
+}
+
+while(<STDIN>) {
     my $thr;
     my $line=$.;
     if (m/(?<DATE>$dateTime)\s+(?<PID>\d+)\s+(?<TID>\d+) (?<LVL>$logLvl) (?<TAG>.*?): (?<CONTENT>.*)$/) {
         my %h=%+;
         $thr = $h{PID} == $h{TID} ? "UI" : "W" . tidChar $h{TID};
-        say qq[$colors{$h{LVL}}$h{DATE}:$thr: $h{TAG}|$h{CONTENT}$colors{N}];
+        my $color = $colors{$h{LVL}};
+        if ($focus_pid && $h{PID} != $focus_pid) {
+            $color = $colors{V}
+        }
+        say qq[$color$h{DATE}:$thr:] . colorize($colors{b}, "$h{TAG}|") . colorize($color, "$h{CONTENT}");
+    } else {
+        say
     }
 }
