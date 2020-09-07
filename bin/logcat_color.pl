f277a612dc04411dc2296b191a4a374faaf46e3c
Vu Tran Kien <vutrankien.vn@gmail.com>
2020-09-07 18:59:55 +0900

    Add grep,start_from function to logcat_color.pl

--- bin/logcat_color.pl
+++ bin/logcat_color.pl
@@ -1,7 +1,8 @@
-#!perl 
+#!/usr/bin/perl 
 use v5.14;
 use List::Util qw(sum);
 use warnings;
+use Getopt::Std;
 
 my $c_markstart="\e[?7711l";
 my $c_markend="\e[?7711h";
@@ -16,7 +17,6 @@ my %colors = (
 );
 my $dateTime=q/[\d-]+ [\d:\.]+/;
 my $logLvl=q/[VDIWE]/;
-my $focus_pid=$ARGV[0];
 
 sub tidChar {
 	my $tid=$_[0];
@@ -29,18 +29,56 @@ sub colorize {
     $color . $_[1] . $colors{n}
 }
 
+my $focus_pid;
+
+#Getopt::Mixed::init('f=i focuspid>f');
+#while( my( $option, $value, $pretty ) = Getopt::Mixed::nextOption() ) {
+#    OPTION: {
+#      $option eq 'f' and do {
+#        $focus_pid = $value if $value;
+#
+#        last OPTION;
+#      };
+#    }
+#  }
+#Getopt::Mixed::cleanup();
+#die "No project specified via -j.\n" unless $Project;
+my %opts;
+getopts('f:s:g:', \%opts);
+#sub HELP_MESSAGE {}
+
+$focus_pid=$opts{f};
+my $startr = $opts{s};
+my $grep = $opts{g};
+my $started = !$startr;
+my $grep2;
+
 while(<STDIN>) {
     my $thr;
-    my $line=$.;
-    if (m/(?<DATE>$dateTime)\s+(?<PID>\d+)\s+(?<TID>\d+) (?<LVL>$logLvl) (?<TAG>.*?): (?<CONTENT>.*)$/) {
+    my $line=$_;
+    my $lineno=$.;
+    if (!$started && $line =~ m/$startr/) {
+        say "started";
+        $started = 'true';
+        if ($1) {
+            say "grep2 = $1";
+            $grep2 = $1
+        }
+    }
+    my $print = $started;
+    #$print = !$grep2 || $line =~ m/$grep2/;
+    $print = !$grep || $line =~ m/$grep/;
+    #say "print = $print";
+    if ($started && $print
+     && $line =~ m/(?<DATE>$dateTime)\s+(?<PID>\d+)\s+(?<TID>\d+) (?<LVL>$logLvl) (?<TAG>.*?): (?<CONTENT>.*)$/) {
         my %h=%+;
-        $thr = $h{PID} == $h{TID} ? "UI" : "W" . tidChar $h{TID};
         my $color = $colors{$h{LVL}};
         if ($focus_pid && $h{PID} != $focus_pid) {
             $color = $colors{V}
         }
+        $thr = $h{PID} == $h{TID} ? "$colors{b}$h{PID}$color" : "$h{PID}" . tidChar $h{TID};
         say qq[$color$h{DATE}:$thr:] . colorize($colors{b}, "$h{TAG}|") . colorize($color, "$h{CONTENT}");
-    } else {
-        say
-    }
+    }# else {
+    #    say
+    #}
 }
